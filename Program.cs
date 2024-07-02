4c2b426c2e5e25a5f94ad0754d7810169ffcb361
Pozzame <pozzame@gmail.com>
2024-07-02 11:39:36 +0200

    Ok

--- Program.cs
+++ Program.cs
@@ -1,2 +1,12 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Random rng = new Random();
+int sum = 0;
+for ( int i = 0 ; i < 10 ; i++ ) sum =+ rng.Next(10);
+Console.WriteLine(sum);
+/*
+Random rng = new Random();
+int sum = 0;
+for (int i = 0;i<10;i++) 
+{int num =rng.Next(10);
+sum += num;
+    Console.WriteLine( num +" "+ sum );}
+Console.WriteLine(sum);*/
