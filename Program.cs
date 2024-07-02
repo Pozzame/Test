98a98c17b9b0bfe1d9c4c870626c41f0e3668f15
Pozzame <pozzame@gmail.com>
2024-07-02 16:04:57 +0200

    Ok

--- Program.cs
+++ Program.cs
@@ -1,13 +1,13 @@
 ﻿/*Random rng = new Random();
 int sum = 0;
-for ( int i = 0 ; i < 10 ; i++ ) sum =+ rng.Next(10);
+for ( int i = 0 ; i < 10 ; i++ ) sum =+ rng.Next(11);
 Console.WriteLine(sum);*/
 /*
 Random rng = new Random();
 int sum = 0;
 for (int i = 0;i<10;i++) 
 {
-    int num =rng.Next(10);
+    int num =rng.Next(11);
     sum += num;
     Console.WriteLine( num +" "+ sum );
 }
